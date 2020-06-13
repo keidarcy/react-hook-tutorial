ca4fdbf50bcfec69ff9d81a30c3148cf91d532a9
XING YAHAO <keidarcy.1015@gmail.com>
2020-06-13 20:14:33 +0900

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import React, {
   useRef,
   useLayoutEffect,
   useCallback,
-  useMemo
+  useMemo,
+  useReducer
 } from 'react'
 import Tool from './Tool'
 import useForm from './useForm'
@@ -20,13 +21,44 @@ const Counter = React.memo(({ onClick, name }) => {
   )
 })
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'input':
+      return {
+        todos: state.todos,
+        text: action.payload
+      }
+    case 'add':
+      return {
+        todos: [...state.todos, { thing: action.payload, completed: false }],
+        text: ''
+      }
+    case 'complete':
+      return {
+        todos: state.todos.map((t, idx) =>
+          idx === action.payload
+            ? { thing: t.thing, completed: !t.completed }
+            : t
+        ),
+        text: state.text
+      }
+    default:
+      break
+  }
+}
+
 function App() {
   // const [form, setForm] = useState({ username: 'yoyo', password: '' })
   const [form, handleChange] = useForm({ username: 'yoyo', password: '' })
   const [showTool, setShowTool] = useState(false)
   const [emoji, setEmoji] = useState('🥳')
 
-  const [counter, setCounter] = useState(0)
+  // const [counter, setCounter] = useState(0)
+  // const [counter, dispatch] = useReducer(reducer, 0)
+  const [{ todos, text }, dispatch] = useReducer(reducer, {
+    todos: [],
+    text: ''
+  })
   const nameRef = useRef()
 
   useEffect(() => {
@@ -38,20 +70,48 @@ function App() {
     setEmoji('😍')
   }, [])
 
-  const onClick = useCallback(() => {
-    setCounter((c) => c + 1)
-  }, [setCounter])
+  // const onClick = useCallback(() => {
+  //   setCounter((c) => c + 1)
+  // }, [setCounter])
 
-  const bigNum = useMemo(() => {
-    let num = 0
-    for (let i = 0; i < 800000000; i++) {
-      num++
-    }
-    return num
-  }, [])
+  // const bigNum = useMemo(() => {
+  //   let num = 0
+  //   for (let i = 0; i < 800000000; i++) {
+  //     num++
+  //   }
+  //   return num
+  // }, [])
 
   return (
     <>
+      {/* <h1>{counter}</h1>
+      <button onClick={() => dispatch({ type: 'increment' })}>increment</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>decrement</button> */}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          dispatch({ type: 'add', payload: text })
+        }}
+      >
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => dispatch({ type: 'input', payload: e.target.value })}
+        />
+      </form>
+      <ul>
+        {todos.map((t, idx) => (
+          <li
+            key={idx}
+            style={{ textDecoration: t.completed ? 'line-through' : '' }}
+            onClick={() => {
+              dispatch({ type: 'complete', payload: idx })
+            }}
+          >
+            {t.thing}
+          </li>
+        ))}
+      </ul>
       <Input
         type="text"
         name="username"
@@ -72,10 +132,10 @@ function App() {
         name="password"
         value={form.password}
       />
-      <h1>
+      {/* <h1>
         {counter} - {bigNum}
       </h1>
-      <Counter onClick={onClick} name={'增加'} />
+      <Counter onClick={onClick} name={'增加'} /> */}
       <div>
         <button onClick={() => setShowTool(!showTool)}>召唤</button>
       </div>
